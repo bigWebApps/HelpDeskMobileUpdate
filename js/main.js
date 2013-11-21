20b88c9cc9c5bb823ee2bba3b35e0856b44a6cef
Eugene <jtrue@mail.ru>
2013-11-21 19:38:30 +0200

    Tkt 1403: App issue

--- js/main.js
+++ js/main.js
@@ -77,15 +77,22 @@ var SherpaDesk = {
 										else 
 										{var header = config.org + '-' + config.inst + ':' + config.apiKey;};		
 		var url = config.url;
-		if( apimethod != '' ){var methodType = apimethod;} else {var methodType = 'GET';};
-        
+		var limit = ""; 
+		if( apimethod ){var methodType = apimethod;} 
+    else {var methodType = 'GET'; 
+          if (method.indexOf("limit") < 0) 
+          {
+           if (method.indexOf("?") != -1) limit = "&"; else limit = "?"; 
+           limit += "limit=200";
+          }
+        };  
         return $.ajax({
             type: methodType,
             beforeSend: function (xhr) {
                 xhr.withCredentials = true;
                 xhr.setRequestHeader('Authorization', 'Basic ' + btoa(header));
                 },
-            url: url + method,
+            url: url + method + limit,
             cache: false,
             error: function(){onOffline();},
             data: data, 
