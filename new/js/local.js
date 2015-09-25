a3fa15e9d2e9e5836489531b21267cfbb3687f25
Eugene <jtrue@mail.ru>
2015-09-25 11:16:30 +0300

    added orange sd

---
+++ new/js/local.js
@@ -0,0 +1,16 @@
+function default_redirect (is_tech)
+{
+    //window.location = is_tech ? "dashboard.html" : "ticket_list.html";
+}
+
+function googleTag() {}
+
+function googleConversion() {}
+
+//Root Names
+var Site = 'bigwebapps.com/';
+var MobileSite = 'http://m.' + Site;
+var AppSite = 'https://app.' + Site;
+var ApiSite = 'http://api.' + Site;
+
+var isSD = false;
